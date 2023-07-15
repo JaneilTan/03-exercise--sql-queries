a328b885182576c490599cd770c85165e00c907d
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-07-15 16:32:23 +1200

    Part B test pass,written SQL queries, save results in .json file

--- Part-B/4-select-col-where.sql
+++ Part-B/4-select-col-where.sql
@@ -1,1 +1,2 @@
 -- Task: Select only the title column and movies that are on Hulu. --
+SELECT title FROM movies WHERE on_hulu;
--- Part-B/5-select-cols-where.sql
+++ Part-B/5-select-cols-where.sql
@@ -1,1 +1,2 @@
 -- Task: Get the title, year, and IMDB rating columns of all movies from the year 1984. --
+SELECT title, year, imdb_rating FROM movies WHERE year = 1984;
